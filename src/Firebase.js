cad93aedf4b487f1b5f7ddb4894109da729d2303
natneli <natanhaileselassie@gmail.com>
2024-02-26 18:18:32 -0700

    First Commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -30,6 +30,16 @@ import "firebase/compat/firestore";
 //  import {initializeApp} from "firebase/app";
 //  import {getAuth }from 'firebase/auth';
 
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyBK5qlOVDVqzbghrWpYWhW9uULU3Ur1rhM",
   authDomain: "nat-4ed21.firebaseapp.com",
@@ -39,6 +49,7 @@ const firebaseConfig = {
   appId: "1:332182006511:web:0790bbabb539b03dfac91a",
   measurementId: "G-3B27E44T88",
 };
+
 // const firebaseConfig = {
 //   apiKey: "AIzaSyD-xL007ekVk9v0N20uRX6V55qLp8EkpR0",
 //   authDomain: "nat-clone.firebaseapp.com",
